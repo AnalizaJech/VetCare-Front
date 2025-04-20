83f5bc09a80b9e0555e03ad70499fea822ca86af
Analiza Jech <2101010055@undc.edu.pe>
2025-04-20 13:39:02 -0500

    fix: router module

--- src/app/pages/landing/components/hero/hero.component.ts
+++ src/app/pages/landing/components/hero/hero.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [RouterLink],
+  imports: [RouterModule],
   templateUrl: './hero.component.html'
 })
 export class HeroComponent {
